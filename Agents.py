8b881496eaedc35b21314eb9125a09fb4ecb2b51
Pablo Ubilla <pabloubilla1998@gmail.com>
2022-05-25 04:07:33 +0200

    agent + experiment
    the general idea of the algorithm, missing a good network probably

---
+++ Agents.py
@@ -0,0 +1,54 @@
+import numpy as np
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv3D, MaxPooling3D, Conv2D, MaxPooling2D, Flatten, Dense
+import tensorflow_probability as tfp
+
+
+
+
+initializer = tf.keras.initializers.Zeros()
+
+class MonteCarloG:
+    
+    def __init__(self, state_shape, n_actions, learning_rate, gamma):
+        self.state_shape = state_shape
+        self.n_actions = n_actions
+        self.learning_rate = learning_rate
+        self.gamma = gamma
+        
+        # check model, maybe get it out of class ?
+        self.model = Sequential()
+        self.model.add(Conv2D(32, (3, 3), activation='relu', input_shape=state_shape))
+        self.model.add(MaxPooling2D((2, 2)))
+        self.model.add(Conv2D(64, (3, 3), activation='relu'))
+        self.model.add(MaxPooling2D((2, 2)))
+        self.model.add(Conv2D(64, (3, 3), activation='relu'))
+        self.model.add(Flatten())
+        self.model.add(Dense(64, activation='relu', kernel_initializer=initializer))
+        self.model.add(Dense(n_actions, activation='softmax'))
+        self.model.compile(loss='categorical_crossentropy', optimizer=tf.keras.optimizers.Adam(learning_rate))
+        
+        
+    def select_action(self, s):
+   
+        probs = self.model.predict(np.array([s]),verbose=0)[0]
+        #print(probs)
+        a = np.random.choice(self.n_actions, p=probs)
+        return a, probs
+        
+    def update(self, r, a, s):
+        G = 0
+        G_list = []
+        for t in range(len(r)-1,-1,-1):
+            
+            G += r[t]
+            G_list.append(G)
+            with tf.GradientTape() as tape:
+                p = self.model(np.array([s[t]]), training=True)
+                loss = G*tfp.distributions.Categorical(probs=p[0]).log_prob(a[t])
+            grads = tape.gradient(loss, self.model.trainable_variables)
+            self.model.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
+          
--- Experiment.py
+++ Experiment.py
@@ -1,21 +1,54 @@
+import numpy as np
 from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
-from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
-
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT, RIGHT_ONLY
+from gym.wrappers import GrayScaleObservation
+from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
+from Agents import MonteCarloG
 
 def main():
     env = gym_super_mario_bros.make('SuperMarioBros-v0')
     env = JoypadSpace(env, SIMPLE_MOVEMENT)
-    print(SIMPLE_MOVEMENT)
-    done = True
-    for step in range(5000):
-        if done:
-            state = env.reset()
-        state, reward, done, info = env.step(env.action_space.sample())
-        env.render()
+    env = gym_super_mario_bros.make('SuperMarioBros-v3')
+    env = JoypadSpace(env, SIMPLE_MOVEMENT)
+    env = GrayScaleObservation(env, keep_dim=True)
+    
+    n_time_steps = 1000
+    gamma = 0.99
+    learning_rate = 0.01
 
-    env.close()
 
+    pi = MonteCarloG(env.observation_space.shape , env.action_space.n, learning_rate, gamma)
+
+    for episodes in range(10):
+        s = env.reset()
+        s_list = []
+        r_list = []
+        a_list = []     
+        done = False
+        for i in range(1000):
+            
+            a, p = pi.select_action(s)
+            
+            # check p to see how it changes
+            if i==0:
+                print(p)
+        
+            s_next, r, done, info = env.step(a)
+            #env.render()
+            s_list.append(s)
+            a_list.append(a)
+            r_list.append(r)
+    
+    
+            if done or i==999:
+                print('done')
+                pi.update(r_list,a_list,s_list)
+                print(np.sum(r_list))
+                break
+                
+            else:
+                s = s_next
 
 if __name__ == '__main__':
     main()
