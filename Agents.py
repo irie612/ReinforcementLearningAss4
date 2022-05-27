c4ebab3e37498cb699d741c928808f0efc6d6df3
irie612 <57913132+irie612@users.noreply.github.com>
2022-05-27 22:58:19 +0200

    Final commit

--- Agents.py
+++ Agents.py
@@ -1,21 +1,23 @@
-import numpy as np
 import os
-import tensorflow as tf
+import numpy as np
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.layers import Dense
 import tensorflow_probability as tfp
+import tensorflow as tf
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
 
 
-def get_model(input, output, lr):
+def get_model(input_shape, output, lr):
     model = Sequential()
-    model.add(tf.keras.Input(shape=input))
+    model.add(tf.keras.Input(shape=input_shape))
     model.add(Dense(12, activation='relu'))
     model.add(Dense(24, activation='relu'))
     model.add(Dense(12, activation='relu'))
     model.add(Dense(output, activation='softmax'))
     model.compile(optimizer=tf.keras.optimizers.Adam(lr))
     return model
+
 
 class MonteCarloG:
 
@@ -24,50 +26,25 @@ class MonteCarloG:
         self.n_actions = n_actions
         self.learning_rate = learning_rate
         self.gamma = gamma
-
-        # check model, maybe get it out of class ?
         self.model = get_model(state_shape, n_actions, learning_rate)
-        
-        # self.model.add(Conv2D(32, (3, 3), activation='relu', input_shape=state_shape))
-        # self.model.add(MaxPooling2D((2, 2)))
-        # self.model.add(Conv2D(64, (3, 3), activation='relu'))
-        # self.model.add(MaxPooling2D((2, 2)))
-        # self.model.add(Conv2D(64, (3, 3), activation='relu'))
-        # self.model.add(Flatten())
-        # self.model.add(Dense(64, activation='relu', kernel_initializer=initializer))
-        # self.model.add(Dense(32, activation='relu', kernel_initializer=initializer))
-        # self.model.add(Dense(n_actions, activation='softmax'))
-        # self.model.compile(loss='categorical_crossentropy', optimizer=tf.keras.optimizers.Adam(learning_rate))
 
     def select_action(self, s):
-        probs = self.model.predict(np.array([s]), verbose=0)[0]#, verbose=0)[0]
-        # print(probs)
+        probs = self.model.predict(np.array([s]), verbose=0)[0]
         a = np.random.choice(self.n_actions, p=probs)
         return a, probs
 
     def update(self, r, a, s):
         G = 0
         G_list = []
-        # for t in range(0, len(r)):
         for t in range(len(r) - 2, -1, -1):
             G = r[t+1] + G * self.gamma
-            G_list.insert(0,G)
-            # with tf.GradientTape() as tape:
-            #     p = self.model(np.array([s[t]]), training=True)
-            #     loss = -self.gamma**t * G * tfp.distributions.Categorical(probs=p[0]).log_prob(a[t])
-            # grads = tape.gradient(loss, self.model.trainable_variables)
-            # self.model.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
-        #print(G_list)
-        #G_list = np.array(G_list)
-        #G_list = ( G_list - np.std(G_list) ) / np.mean(G_list)
+            G_list.insert(0, G)
         for t in range(len(r)-1):
             with tf.GradientTape() as tape:
-                p = self.model(np.array([s[t]])) 
-                #if t==1:
-                    #print(s[t],a[t], tfp.distributions.Categorical(probs=p[0]).log_prob(a[t]))
+                p = self.model(np.array([s[t]]))
                 loss = -self.gamma**t * G_list[t] * tfp.distributions.Categorical(probs=p[0]).log_prob(a[t])
             grads = tape.gradient(loss, self.model.trainable_variables)
-            # if t==5:
-            #     print(grads)
             self.model.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
-            
+
+    def save_model(self, name):
+        self.model.save(f'saved_models/{name}')
--- Experiment.py
+++ Experiment.py
@@ -1,70 +1,81 @@
-import gym
+from multiprocessing import Pool, cpu_count
 import numpy as np
-from nes_py.wrappers import JoypadSpace
-import gym_super_mario_bros
-from gym.wrappers import GrayScaleObservation
-from Agents import MonteCarloG
-from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+from Agents import MonteCarloG  
+import gym
+from Helper import smooth, plot_colormap, plot_reward_graph
+from tqdm import tqdm
 
 
-class SkipFrame(gym.Wrapper):
-    def __init__(self, env, n_frames):
-        super().__init__(env)
-        self.n_frames = n_frames
-
-    def step(self, action):
-        total_reward = 0
-        done = False
-        for i in range(self.n_frames):
-            state, reward, done, info = self.env.step(action)
-            total_reward += reward
-            if done:
-                break
-        return state, total_reward, done, info
-
-
-def main():
-    # env = gym_super_mario_bros.make('SuperMarioBros-v0')
-    # env = JoypadSpace(env, SIMPLE_MOVEMENT)
-    env = gym_super_mario_bros.make('SuperMarioBros-v3')
-    env = JoypadSpace(env, SIMPLE_MOVEMENT)
-    env = GrayScaleObservation(SkipFrame(env, n_frames=4), keep_dim=True)
-    # env = FrameStack(ResizeObservation(GrayScaleObservation(SkipFrame(env, n_frames=4)), shape=84), num_stack=4)
-
-    n_time_steps = 1000
-    gamma = 0.99
-    learning_rate = 0.001
-
-    pi = MonteCarloG(env.observation_space.shape, env.action_space.n, learning_rate, gamma)
-
-    for episodes in range(10):
+def run_episodes(agent, env_name, n_episodes, alpha, gamma, max_steps):
+    rewards_over_episodes = np.zeros(shape=n_episodes)
+    env = gym.make(env_name)
+    pi = agent(env.observation_space.shape, env.action_space.n, alpha, gamma)
+    for e in range(n_episodes):
         s = env.reset()
         s_list = []
         r_list = []
         a_list = []
-        done = False
-        for i in range(100):
-
+        for i in range(max_steps):
             a, p = pi.select_action(s)
-
-            # check p to see how it changes
-            if i==0:
-                print(p)
-
             s_next, r, done, info = env.step(a)
-            # env.render()
             s_list.append(s)
             a_list.append(a)
             r_list.append(r)
-
-            if done or i == 99:
-                print('done')
-                pi.update(r_list,a_list,s_list)
-                print(np.sum(r_list))
+            if done or i == max_steps - 1:
+                pi.update(r_list, a_list, s_list)
+                rewards_over_episodes[e] = np.sum(r_list)
                 break
-
             else:
                 s = s_next
+    return rewards_over_episodes
+
+
+def run_repetitions(agent, env_name, n_repetitions, n_episodes, smoothing_window, alpha, gamma, max_steps):
+    learning_curve = np.zeros(shape=(n_repetitions, n_episodes))
+    for rep in range(n_repetitions):
+        print(f'Repetition {rep} ' + agent.__name__)
+        learning_curve[rep] = run_episodes(agent, env_name, n_episodes, alpha, gamma, max_steps)
+    learning_curve = np.mean(learning_curve, axis=0)
+    learning_curve = smooth(learning_curve, smoothing_window)
+    return learning_curve
+
+
+def run_repetitions_parallel(agent, env_name, n_reps, n_eps, smoothing_win, alpha, gamma, max_steps):
+    async_results = []
+    pool = Pool(processes=cpu_count())
+    for i in range(n_reps):
+        async_results.append(pool.apply_async(run_episodes, args=(agent, env_name, n_eps, alpha, gamma, max_steps)))
+    pool.close()
+    pool.join()
+    learning_curve = np.mean([r.get() for r in async_results], axis=0)
+    learning_curve = smooth(learning_curve, smoothing_win)
+    return learning_curve
+
+
+def run_experiment(alphas, gammas, agent, env_name, n_reps, n_eps, smoothing_win, max_steps, parallel=True):
+    means = np.zeros(shape=(len(alphas), len(gammas), n_eps))
+    for i, alpha, in enumerate(alphas):
+        for j, gamma in enumerate(tqdm(gammas, desc=f"{agent.__name__} in {env_name} for {alpha=} over {gammas=}")):
+            if parallel:
+                means[i, j] = run_repetitions_parallel(agent, env_name, n_reps, n_eps, smoothing_win, alpha, gamma, max_steps)
+                np.save(f"arrays/{alpha}_{gamma}", means[i, j])
+            else:
+                means[i, j] = run_repetitions(agent, env_name, n_reps, n_eps, smoothing_win, alpha, gamma, max_steps)
+    return means
+
+
+def main():
+    env_name = 'CartPole-v1'
+    agent = MonteCarloG
+    gammas = [0.9, 0.95, 0.99, 1]
+    alphas = [0.00001, 0.0001, 0.001, 0.01]
+    max_steps = 50
+    n_repetitions = 5
+    n_episodes = 200
+    smoothing_window = 11
+
+    means = run_experiment(alphas, gammas, agent, env_name, n_repetitions, n_episodes, smoothing_window, max_steps)
+    np.save("arrays/means", means)
 
 
 if __name__ == '__main__':
